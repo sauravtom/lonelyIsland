bf6fd1c55deac062c3d85280939c203bcc3683d0
Archit Verma <architv07@gmail.com>
2015-01-15 15:58:38 +0000

    implemented login, logout

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
 
 import flask, flask.views
-from settings import app
-
-from flask import render_template
-from flask import request
-from flask.ext.login import LoginManager
+from settings import app, login_manager
+from flask import render_template, request, url_for, abort, g
 import json
 import os
 from dbHelper import DB
 from models import *
+from flask.ext.login import login_user , logout_user , current_user , login_required
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.route("/login", methods=["GET"])
+def login():
+    user_name = request.args.get('user_name') or ''
+    user_password = request.args.get('user_password') or ''
+    if user_name and user_password:
+        user_exists = User.query.filter_by(username=user_name,password=user_password).first()
+        if user_exists: # user authenticated
+            login_user(user_exists, remember=True)
+            return flask.redirect(url_for('home'))
+        else:
+            registered_user = User.query.filter_by(username=user_name).first()
+            if registered_user: # authentication failed
+                return render_template('login.html')
+            else: # new user
+                user_to_be_added = User(user_password, user_name)
+                db.session.add(user_to_be_added)
+                db.session.commit()
+                login_user(user_to_be_added, remember=True)
+                return flask.redirect(url_for('home'))
+    return render_template("login.html")
 
 @app.route('/')
+@login_required
 def home():
     arr = Post.query.filter_by(is_deleted=0)
+    print arr
     return flask.render_template('index.html',arr=arr,mode='normal')
 
 @app.route('/view/<id>')
+@login_required
 def view(id):
     post = Post.query.filter_by(id=id).first()
     comments = Comment.query.filter_by(post_id=id)
@@ -24,6 +50,7 @@ def view(id):
 
 
 @app.route('/add',methods=['GET'])
+@login_required
 def add():
     post_content = request.args.get('post_content')
     user_id = request.args.get('user_id')
@@ -33,6 +60,7 @@ def add():
     return flask.redirect("/#sa")
 
 @app.route('/add_comment',methods=['GET'])
+@login_required
 def add_comment():
     comment = request.args.get('comment')
     post_id = request.args.get('post_id')
@@ -43,6 +71,7 @@ def add_comment():
     return flask.redirect("/#sa")
 
 @app.route('/edit',methods=['GET'])
+@login_required
 def edit():
     post_id = request.args.get('post_id')
     action = request.args.get('action')
@@ -70,6 +99,10 @@ def edit():
 
     return flask.redirect("/#error")
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return flask.redirect(url_for('login')) 
 
 if __name__ == '__main__':
     app.debug = True
--- models.py
+++ models.py
@@ -26,22 +26,22 @@ class User(TimeStampedModel):
 		self.post_ids = post_ids
 
 	def is_authenticated(self):
-        return True
+		return True
 
-    def is_active(self):
-        return True
+	def is_active(self):
+		return True
 
-    def is_anonymous(self):
-        return False
+	def is_anonymous(self):
+		return False
 
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
+	def get_id(self):
+		try:
+			return unicode(self.id)  # python 2
+		except NameError:
+			return str(self.id)  # python 3
 
-    def __repr__(self):
-        return '<User %r>' % (self.username)
+	def __repr__(self):
+		return '<User %r>' % (self.username)
 
 class Post(TimeStampedModel):
 	__tablename__ = 'posts'
