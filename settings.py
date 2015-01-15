b58d8ebfc9ae52a08e710918706365b4e1fe97eb
Archit Verma <architv07@gmail.com>
2015-01-15 14:03:31 +0000

    created post page, comments page, feed page

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////vagrant/projects/lonelyIsland/data.db'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
