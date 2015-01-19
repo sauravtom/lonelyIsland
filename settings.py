10002556fc2188fee9bc45b97f7a68f9cf3b7b20
Archit Verma <architv07@gmail.com>
2015-01-19 12:40:24 +0000

    changes in UI

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 import flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+import os
 
+#print os.getcwd()
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/saurav/Desktop/lonelyIsland/data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s/data.db'%os.getcwd()
 app.secret_key = 'thisisarandomstring007becauseilovejamesbond'
 app.config['SESSION_TYPE'] = 'filesystem'
 db = SQLAlchemy(app)
